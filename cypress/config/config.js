7b6cdeea41533f5c1dcd55138a40f596d91fcc1f
Dejana Ristić <63868615+dejana-ferit99@users.noreply.github.com>
2024-07-16 11:20:15 +0200

    Update config.js

--- cypress/config/config.js
+++ cypress/config/config.js
@@ -1,7 +1,7 @@
 //register on: https://parabank.parasoft.com/parabank/register.htm
 //after successful registration, enter username and password to successfully run tests
 module.exports = {
-    username: 'user60',
+    username: 'nikola_tesla',
     password: '1234567',
 };
 
